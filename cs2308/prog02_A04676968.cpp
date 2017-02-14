6f2ebf1b1e9d78f3b5f1c65afa09f2256a3ae513
cicoyle <cic26@txstate.edu>
2017-02-13 22:13:00 -0600

    Fixed error. Code runs and works.

--- cs2308/prog02_A04676968.cpp
+++ cs2308/prog02_A04676968.cpp
@@ -16,7 +16,7 @@ using namespace std;
 
 //prototypes
 int validatefiles();
-char * getToken(char * studentInfo, int position, char * returnedWord);
+char *getToken(char *studentInfo, int position, char *returnedWord);
 
 ifstream fin;
 ofstream fout;
@@ -30,10 +30,10 @@ int main()
 	validatefiles();
 	//variables 
 	char studentInfo[256];
-	char * returnedWord = new char[256];
-	int position = 0; //word I'm looking for
-	char firstname[11];
-	char lastname[13];
+	char *returnedWord = new char[256];
+	int position = 0;	//word I'm looking for
+	char *firstname;
+	char *lastname;
 	int ID = 0;
 	int grade1 = 0;
 	int grade2 = 0;
@@ -42,23 +42,18 @@ int main()
 	int grade5 = 0;
 	int grade6 = 0;
 
-
 	//read in and assign words to arrays while not end of file
-	while(!fin.eof())
-	{
-		
-		fin.getline(studentInfo, 256);
-		cout << studentInfo << endl;	
-		
-		firstname =      getToken(studentInfo, 0, returnedWord);	
-		lastname  =      getToken(studentInfo, 1, returnedWord);	
-		ID        = atoi(getToken(studentInfo, 2, returnedWord));	
-		grade1    = atoi(getToken(studentInfo, 3, returnedWord));	
-		grade2    = atoi(getToken(studentInfo, 4, returnedWord));	
-		grade3    = atoi(getToken(studentInfo, 5, returnedWord));	
-		grade4    = atoi(getToken(studentInfo, 6, returnedWord));	
-		grade5    = atoi(getToken(studentInfo, 7, returnedWord));	
-		grade6    = atoi(getToken(studentInfo, 8, returnedWord));	
+	while (fin.getline(studentInfo, 256)) {
+
+		firstname = getToken(studentInfo, 0, returnedWord);
+		lastname = getToken(studentInfo, 1, returnedWord);
+		ID = atoi(getToken(studentInfo, 2, returnedWord));
+		grade1 = atoi(getToken(studentInfo, 3, returnedWord));
+		grade2 = atoi(getToken(studentInfo, 4, returnedWord));
+		grade3 = atoi(getToken(studentInfo, 5, returnedWord));
+		grade4 = atoi(getToken(studentInfo, 6, returnedWord));
+		grade5 = atoi(getToken(studentInfo, 7, returnedWord));
+		grade6 = atoi(getToken(studentInfo, 8, returnedWord));
 	}
 	//close files
 	fin.close();
@@ -66,58 +61,59 @@ int main()
 	return 0;
 
 }
+
 //function validate files
 int validatefiles()
-{	
+{
 	//check that input file opened
-	if(!fin)
-	{
+	if (!fin) {
 		cout << "Error! Input file did not open.";
 		return 1;
 	}
 	//check that output file opened
-	if(!fout)
-	{
+	if (!fout) {
 		cout << "Error! Output file did not open.";
 		return 1;
 	}
-	
+
 }
+
 //function get token
-char * getToken(char * studentInfo, int position, char * returnedWord)
-{	
+char *getToken(char *studentInfo, int position, char *returnedWord)
+{
 	//variables
-	char * iterator = studentInfo;
-	int count = 0; //this is the current word
-	char space = 32; 
-	char tab = 9; 
+	char *iterator = studentInfo;
+	int count = 0;		//this is the current word
+	char space = 32;
+	char tab = 9;
 	int singleLetter = 0;
-	//do all this at least once while iterator increments and count	is not position	
+	//do all this at least once while iterator increments and count is not position 
 	do {
 		//if iterator is a space or tab increment count
-		if(* iterator == space || * iterator == tab)
+		if (*iterator == space || *iterator == tab)
 			count++;
+
 		//while iterator is a space or tab increment iterator
-		while(* iterator == space || * iterator == tab)
+		while (*iterator == space || *iterator == tab)
 			iterator++;
-	} while(* iterator++ && count != position);
+
+	} while (*iterator++ && count != position);
+
 	//decrement iterator
 	iterator--;
-	//set new iterator to current letter
-	char * originaltoken = iterator;
-	//while iterator is not a space or tab put letters into the returned word array
-	while((* originaltoken != space) && (* originaltoken != tab))
-	{
-		returnedWord[singleLetter++] = * originaltoken;
-		originaltoken++;
 
+	//set new iterator to current letter
+	char *originaltoken = iterator;
+
+	//while iterator is not a space or tab put letters into the returned word array
+	while ((*originaltoken != space) && (*originaltoken != tab)) {
+		returnedWord[singleLetter++] = *originaltoken;
+		originaltoken++;
 	}
+
 	//attach the null terminator to end of returned word array
 	returnedWord[singleLetter++] = '\0';
 	//return returned word array
 	return returnedWord;
-	
+
 }
-
-
-

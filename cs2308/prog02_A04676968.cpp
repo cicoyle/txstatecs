7f010e97dd5f40ee4097a2af63814d405a28b1b3
cicoyle <cic26@txstate.edu>
2017-02-14 00:10:53 -0600

    Fixed min. Ignore dropped grade function.

--- cs2308/prog02_A04676968.cpp
+++ cs2308/prog02_A04676968.cpp
@@ -17,6 +17,9 @@ using namespace std;
 //prototypes
 int validatefiles();
 char *getToken(char *studentInfo, int position, char *returnedWord);
+void dropGrade(int grade1, int grade2, int grade3, int grade4, int grade5, int grade6);
+
+
 
 ifstream fin;
 ofstream fout;
@@ -40,20 +43,59 @@ int main()
 	int grade3 = 0;
 	int grade4 = 0;
 	int grade5 = 0;
-	int grade6 = 0;
-
+	int grade6 = -1;
+	int grades[6] = {-1};
 	//read in and assign words to arrays while not end of file
 	while (fin.getline(studentInfo, 256)) {
 
 		firstname = getToken(studentInfo, 0, returnedWord);
-		lastname = getToken(studentInfo, 1, returnedWord);
-		ID = atoi(getToken(studentInfo, 2, returnedWord));
-		grade1 = atoi(getToken(studentInfo, 3, returnedWord));
-		grade2 = atoi(getToken(studentInfo, 4, returnedWord));
-		grade3 = atoi(getToken(studentInfo, 5, returnedWord));
-		grade4 = atoi(getToken(studentInfo, 6, returnedWord));
-		grade5 = atoi(getToken(studentInfo, 7, returnedWord));
-		grade6 = atoi(getToken(studentInfo, 8, returnedWord));
+		lastname  = getToken(studentInfo, 1, returnedWord);
+		ID        = atoi(getToken(studentInfo, 2, returnedWord));
+		grades[0]    = atoi(getToken(studentInfo, 3, returnedWord));
+		grade1    = atoi(getToken(studentInfo, 3, returnedWord));
+		grades[1]    = atoi(getToken(studentInfo, 4, returnedWord));
+		grade2    = atoi(getToken(studentInfo, 4, returnedWord));
+		grades[2]    = atoi(getToken(studentInfo, 5, returnedWord));
+		grade3    = atoi(getToken(studentInfo, 5, returnedWord));
+		grades[3]    = atoi(getToken(studentInfo, 6, returnedWord));
+		grade4    = atoi(getToken(studentInfo, 6, returnedWord));
+		grades[4]    = atoi(getToken(studentInfo, 7, returnedWord));
+		grade5    = atoi(getToken(studentInfo, 7, returnedWord));
+		grades[5]    = atoi(getToken(studentInfo, 8, returnedWord));
+		grade6    = atoi(getToken(studentInfo, 8, returnedWord));
+
+
+
+	
+		cout << "name: " << getToken(studentInfo, 0, returnedWord) << endl;
+		cout << "Lname: " << getToken(studentInfo, 1, returnedWord) << endl;
+		cout << "ID: " << ID << endl;
+		cout << "g1: " << grade1 << endl;
+		cout << "g2: " << grade2 << endl;
+		cout << "g3: " << grade3 << endl;
+		cout << "g4: " << grade4 << endl;
+		cout << "g5: " << grade5 << endl;
+		cout << "g6: " << grade6 << endl;
+
+int i;
+cout << "loopin biiiiiitch" << endl;
+for(i = 0; i < 6; i++)
+cout << "grade " << i << " is " << grades[i] << endl;
+
+int min = 100;
+for(i = 0; i < 6; i++)
+if(grades[i] < min)
+min = grades[i];
+
+cout << "lowest: " << min << endl;
+		dropGrade(grade1, grade2, grade3, grade4, grade5, grade6);
+
+
+
+
+
+
+
 	}
 	//close files
 	fin.close();
@@ -117,3 +159,30 @@ char *getToken(char *studentInfo, int position, char *returnedWord)
 	return returnedWord;
 
 }
+
+
+//function dropGrade
+void dropGrade(int grade1, int grade2, int grade3, int grade4, int grade5, int grade6)
+{
+	int lowestGrade = grade1;
+
+	if(grade6 != -1) {
+		if(grade2 < lowestGrade)
+			lowestGrade = grade2; 
+		else if(grade3 < lowestGrade)
+			lowestGrade = grade3;
+		else if(grade4 < lowestGrade)
+			lowestGrade = grade4;
+		else if(grade5 < lowestGrade)
+			lowestGrade = grade5;
+		else if(grade6 < lowestGrade)
+			lowestGrade = grade6;
+	}
+	else
+		grade6 = 0;
+	cout << "lowest: " << lowestGrade << endl;
+
+
+
+}
+

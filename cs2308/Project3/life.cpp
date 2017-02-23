241bc82431abff30184269adaa5e1e7d33d398b5
cicoyle <cic26@txstate.edu>
2017-02-22 22:41:05 -0600

    Reads in file correctly. Out puts correct data. :p

--- cs2308/Project3/life.cpp
+++ cs2308/Project3/life.cpp
@@ -18,34 +18,69 @@ using namespace std;
 
 //GLOBAL VARIABLES
 
-int ROWS;         //stores the number of rows in the grid
-int COLUMNS;      //stores the number of rows in the grid
+char **world;
+int height;         //stores the number of rows in the grid
+int width;      //stores the number of rows in the grid
+void prepass(const char * file)
+ {
+	ifstream fin(file);
+	if(!fin)
+		cout << "Error. Input file did not open.";
+
+	char cell;
+	while(fin.get(cell)) {
+		if(cell == 48 || cell == 49)
+		width++;
+		if(cell == '\n')
+			height++;
+	}
+
+	width/=height;
+	fin.close();
+}
 
 //This function reads input file for subsequent prosessing (add high level 
 //description of your implementation logic) 
 void populateWorld (const char * file)
 {
+
+	height = 0;
+	width = 0;
+	prepass(file);
+cout << height << endl << width << endl;	
 	ifstream fin;
-	fin.open("glider_gun_fight.txt");
-	int *array;	
+
+	fin.open(file);
+
 	if(!fin) 
 		cout << "Error. Input file did not open.";
 
-	while(!fin.eof()) {
-		while(fin)
-			COLUMNS++;
-	ROWS++;
-//	array = new int *[ROWS];
-//	for(int row = 0; row < ROWS; row++)
-//		array[row] = new int[COLUMNS];
+	char cell;	    
+	world = new char * [height];
+
+	for(int h = 0; h < height; h++) {
+		world[h] = new char [width];
+		for(int w = 0; w < width; w++)	{
+			fin.get(cell);
+			if(cell == 48 || cell == 49)
+				*(*(world + h) + w) = cell; 
+		}	
 	}
-
+fin.close();
 }
-
+char getsquare(int height, int width) {
+	return *(*(world + height) + width); 
+}
+	
 //This function outputs the grid for current generation (add high level 
 //description of your implementation logic) 
 void showWorld ()
 {
+	for(int h = 0; h < height; h++) 
+		for(int w = 0; w < width; w++)	
+			cout << getsquare(h,w); 
+		
+	
 
 }
 

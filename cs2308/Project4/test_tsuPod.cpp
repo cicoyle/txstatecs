8b33d82e1709849a5f600e611c518cc4ccaac8fd
cicoyle <cic26@txstate.edu>
2017-03-12 22:19:38 -0500

    Project donezo! shuffles songs and all

--- cs2308/Project4/test_tsuPod.cpp
+++ cs2308/Project4/test_tsuPod.cpp
@@ -2,7 +2,7 @@
 Name: Cassandra Coyle
 Date: 3/12/17
 Problem Number:4
-Hours spent solving the problem:
+Hours spent solving the problem: 8
 Instructor: Komogortsev, TSU
 *****************************************************/
 #include <iostream>
@@ -19,45 +19,74 @@ int main()
     // Following code initializes the tsuPod 
     initTsuPod();
 
-    
+    cout << "INITIALIZED PLAYLIST: " << endl;    
     // Following code lists all songs located in tsuPod memory 
-    showSongList(/*add nessesary parameters here*/);
-    cout << endl;
-    
-
-
-    // Following code tests tsuPod input functionality 
-    int retCode = addSong("Runaway1", "Bon Jovi1", 1);
-    retCode = addSong(/*add nessesary parameters here,*/ "Runaway2", "Bon Jovi2", 2);
-    retCode = addSong(/*add nessesary parameters here,*/ "Runaway3", "Bon Jovi3", 3);
-    retCode = addSong(/*add nessesary parameters here,*/ "Runaway4", "Bon Jovi4", 1);
-    retCode = addSong(/*add nessesary parameters here,*/ "Runaway5", "Bon Jovi5", 5);
-    retCode = addSong(/*add nessesary parameters here,*/ "Runaway6", "Bon Jovi6", 6);
-    retCode = addSong(/*add nessesary parameters here,*/ "Runaway7", "Bon Jovi7", 1);
-    retCode = addSong(/*add nessesary parameters here,*/ "Runaway8", "Bon Jovi8", 1);
-    retCode = addSong(/*add nessesary parameters here,*/ "Runaway8", "Bon Jovi8", 1);
-    retCode = addSong(/*add nessesary parameters here,*/ "Runaway8", "Bon Jovi8", 1);
-    retCode = addSong(/*add nessesary parameters here,*/ "Runaway8", "Bon Jovi8", 1);
-    cout << retCode << endl;   
     showSongList();
     cout << endl;
 
+    // adds 8 songs correctly-success
+    // Following code tests tsuPod input functionality 
+    int retCode = addSong("Blue Ain't Your Color", "Keith Urban", 1);
+	cout << "SUCCESS: " << retCode << endl;
+    retCode = addSong("Make Me (Cry)", "Noah Cyrus", 2);
+	cout << "SUCCESS: " << retCode << endl;
+    retCode = addSong("Little Do You Know", "Alex and Sierra", 3);
+	cout << "SUCCESS: " << retCode << endl;
+    retCode = addSong("Caroline", "Amine", 1);
+	cout << "SUCCESS: " << retCode << endl;
+    retCode = addSong("Fake Love", "Drake", 5);
+	cout << "SUCCESS: " << retCode << endl;
+    retCode = addSong("Shape Of You", "Ed Sheeran", 6);
+	cout << "SUCCESS: " << retCode << endl;
+    retCode = addSong("I Hate You I Love You", "Gnash", 1);
+	cout << "SUCCESS: " << retCode << endl;
+    retCode = addSong("No Role Modelz", "J. Cole", 1);
+	cout << "SUCCESS: " << retCode << endl;
 
+    //unsuccess-lacks memory
+    retCode = addSong("Issues", "Julia Michaels", 24);
+	cout << "UNSUCCESS-lacks memory: " << retCode << endl;
+
+    //unsuccess-no empty slots
+    retCode = addSong("Lost Boy", "Ruth B", 1);
+	cout << "UNSUCCESS-no empty slots: " << retCode << endl;
+    retCode = addSong("Don't Tell Nobody", "Tink", 1);
+	cout << "UNSUCCESS-no empty slots: " << retCode << endl;
+
+    
+    cout << "SONGS IN PLAYLIST: " << endl;
+    showSongList();
+    cout << endl;
 
     // Following code tests tsuPod song removal functionality         
-    retCode = removeSong("Runaway100");
-    cout << retCode << endl;    
+    retCode = removeSong("Make Me (Cry)");
+    cout << "SUCCESSFULLY REMOVED: " << retCode << endl; 
+
+    //retcode when song is not in playlist
+    retCode = removeSong("Cookie Monster");
+    cout << "UNSUCCESSFULLY REMOVED-song is not in playlist: " << retCode << endl;  
+ 
+
+    //display playlist when song is removed
+    cout << "PLAYLIST SHOWING REMOVED SONG: " << endl;  
     showSongList();
     cout << endl;
-
 
 
     // Following code tests tsuPod song shuffling functionality      
     shuffle();
     cout << endl;
+
+    //display shuffled playlist
+    cout << "SHUFFLED PLAYLIST: " << endl;
     showSongList();
     cout << endl;
+
+
+   //display cleared playlist
+   cout << "CLEARED PLAYLIST: " << endl;
+   clearMemory();   
+   showSongList();
     
-//    system("PAUSE");
     
 }
--- cs2308/Project4/tsuPod.cpp
+++ cs2308/Project4/tsuPod.cpp
@@ -2,23 +2,22 @@
 Name: Cassandra Coyle
 Date: 3/12/17
 Problem Number: 4
-Hours spent solving the problem:
+Hours spent solving the problem: 8
 Instructor: Komogortsev, TSU
 *****************************************************/
 #include <iostream>
 #include <fstream>
 #include <string>
 #include "tsuPod.h"
-
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 //declare variables globally
 const int NUM_SONGS = 8;
 const int MEMORY_SIZE = 25;
 
-//function initialize tsupod
-void initTsuPod()
-{
 	//create struct
 	struct TsuPod
 	{
@@ -29,49 +28,122 @@ void initTsuPod()
 	
 	//create instance
 	TsuPod tsuPod[NUM_SONGS];
-	
+
+//function initialize tsupod
+void initTsuPod()
+{
 	//initialize
 	for(int i = 0; i < NUM_SONGS; i++) {
 		tsuPod[i].title = " ";
 		tsuPod[i].artist = " ";
 		tsuPod[i].size = 0;
-     	}
-  
+  	}
 }
 
 //function to add song
-int addSong (/*add nessesary parameters here,*/ string newTitle, string newArtist, int size)
+int addSong (string newTitle, string newArtist, int size)
 {
+	//initalize variables
+	 int i = 0;
+    	 int memory = 0;
+   	 int total = 0;
 
+	//calculate total memory    
+	for(int i = 0; i < NUM_SONGS; i++)
+		total += tsuPod[i].size;
+	
+	memory = MEMORY_SIZE - total;
+ 	
+	//if size is bigger than memor
+	if(size > memory)
+		return -1;
+	
+	//assign songs
+	while(i < NUM_SONGS)
+    	{
+           if (tsuPod[i].size > 0)
+              	i++;
+           else {
+               tsuPod[i].title = newTitle;
+               tsuPod[i].artist = newArtist;
+               tsuPod[i].size = size;
+               return 0;
+	   }
+    	}
+    
+	//if more than 8 songs
+	if (i >= 8)
+		return -2;
+}
 
+//function to show song list
+void showSongList()
+{	
+	//display the header
+	cout << setw(10) << left << "Slot";
+	cout << setw(40) << left << "Title";
+	cout << setw(30) << left << "Artist";
+	cout << setw(10) << left << "Size: MB";
+	cout << endl;
+
+	//display songs and slot number
+	for(int i = 0; i < NUM_SONGS; i++) {
+		cout << setw(10) << left << (i+1);
+		cout << setw(40) << left << tsuPod[i].title;
+		cout << setw(30) << left << tsuPod[i].artist;
+		cout << setw(10) << left << tsuPod[i].size;
+		cout << endl;
+	}
 }
 
 //function to remove song
-int removeSong (/*add nessesary parameters here,*/ string title)
+int removeSong (string title)
 {
-
-
+    //remove song if titles match
+    int i = 0;    
+    while (i <= NUM_SONGS - 1) { 
+          if(tsuPod[i].title == title) {
+              tsuPod[i].title = "EMPTY";
+              tsuPod[i].artist = "EMPTY";
+              tsuPod[i].size = 0;
+              return 0;
+              }
+          else
+              i++;
+    }
+    
+    if (i > NUM_SONGS-1)
+       return -1;
 }
 
 //function to clear memory
-void clearMemory(/*add nessesary parameters here*/)
+void clearMemory()
 {
-
-
-}
-
-//function to display song list
-void showSongList (/*add nessesary parameters here*/)
-{
-
-
+	//clear memory
+	for(int i = 0; i < NUM_SONGS; i++) {
+		tsuPod[i].title = " ";
+		tsuPod[i].artist = " ";
+		tsuPod[i].size = 0;
+	}
 }
 
 //function to shuffle songs
-void shuffle (/*add nessesary parameters here*/)
+void shuffle ()
 {
+	//generate seed
+	srand(time(0));
 
+	//variables
+	TsuPod tempVariable;
+	int r = 0;
 
+	//randomly shuffle songs
+	 for(int i = 0; i < NUM_SONGS; i++) {
+            r = rand() % NUM_SONGS; 
+            tempVariable = tsuPod[i];
+	    tsuPod[i] = tsuPod[r];
+	    tsuPod[r] = tempVariable;
+        }
 }
 
 
